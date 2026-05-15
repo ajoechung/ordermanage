a498dee5bf31f80313ae959c7af06415a9209bb6
ajoechung <ajoechung@users.noreply.github.com>
2026-05-15 09:03:10 +0000

    Force update frontend-dist with latest build

--- chemdoc/frontend-dist/assets/captcha-CMGJOJNM.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-CpnQOYeY.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-9EtLyESn.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
