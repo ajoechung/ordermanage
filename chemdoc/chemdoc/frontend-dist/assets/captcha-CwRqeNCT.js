ee5f8428548dcdf7989d65261971184871a18cc3
ajoechung <ajoechung@users.noreply.github.com>
2026-05-16 02:46:34 +0000

    Remove old frontend-dist

--- chemdoc/chemdoc/frontend-dist/assets/captcha-CwRqeNCT.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-Cq_T2qP6.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-ClWoXyQN.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
