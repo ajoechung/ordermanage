3710086baba32590a7147e8f59bff014ac70efc7
ajoechung <ajoechung@users.noreply.github.com>
2026-05-16 02:20:19 +0000

    Fix: 1) Remove alert test code 2) Fix contact phone field reading error 3) Fix follow record save error

---
+++ chemdoc/chemdoc/frontend-dist/assets/captcha-CwRqeNCT.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-Cq_T2qP6.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-ClWoXyQN.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
