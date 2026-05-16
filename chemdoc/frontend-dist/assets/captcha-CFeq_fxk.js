fa388ae25b63d0a67cc4c3c65e09733b6052d856
ajoechung <ajoechung@users.noreply.github.com>
2026-05-16 15:38:29 +0000

    更新前端发布目录

--- chemdoc/frontend-dist/assets/captcha-CFeq_fxk.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-NGw6kmRU.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-ClWoXyQN.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
--- chemdoc/frontend-dist/assets/captcha-CwZPyWi9.js
+++
@@ -1,1 +0,0 @@
-import{s as t}from"./index-BcP-oPfe.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-ClWoXyQN.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
