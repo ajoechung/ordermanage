8560b0d32ee70dfdbc1cb630e2471362697160d9
ajoechung <ajoechung@users.noreply.github.com>
2026-05-16 16:15:50 +0000

    修复路由匹配顺序问题

--- chemdoc/backend/route/app.php
+++ chemdoc/backend/route/app.php
@@ -27,11 +27,11 @@ Route::group('api', function () {
     Route::post('follow', 'api.Follow/save');
     Route::delete('follow/:id', 'api.Follow/delete');
 
-    Route::get('order', 'api.Order/index');
     Route::get('order/:id', 'api.Order/read');
-    Route::post('order', 'api.Order/save');
     Route::put('order/:id', 'api.Order/update');
     Route::delete('order/:id', 'api.Order/delete');
+    Route::get('order', 'api.Order/index');
+    Route::post('order', 'api.Order/save');
     Route::post('order/status', 'api.Order/status');
     Route::get('order/invoices/:orderId', 'api.Order/invoices');
     Route::post('order/uploadInvoice', 'api.Order/uploadInvoice');
