4c11aca6697cf01bc69010c0be9cea839777306a
Solo Agent <solo@chemdoc.com>
2026-05-18 05:37:42 +0000

    修复客户详情路由问题，更新为 customer/full-detail/:id

---
+++ chemdoc/frontend/dist/assets/captcha-D7oIDw1X.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-ul_mo7uf.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-Cmtp2jkN.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
