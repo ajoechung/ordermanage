f26d235a8675255c801840e3dd740bb0707f93b1
ajoechung <ajoechung@users.noreply.github.com>
2026-05-21 03:18:43 +0000

    前端代码打包完成

---
+++ chemdoc/frontend/dist/assets/captcha-DPFzy_zD.js
@@ -0,0 +1,1 @@
+import{s as t}from"./index-CsAKXoaB.js";import"./vue-core-D7ZKWQmy.js";import"./element-plus-BamIGOgJ.js";function p(){return t({url:"/captcha",method:"get"})}export{p as getCaptcha};
