5653416725a3c3beadc540429354c9f676f62299
ajoechung <ajoechung@users.noreply.github.com>
2026-05-28 07:24:14 +0000

    feat: 化工单据管理系统需求梳理
    Co-authored-by: traeagent <traeagent@users.noreply.github.com>

--- chemdoc/backend/vendor/composer/autoload_psr4.php
+++ chemdoc/backend/vendor/composer/autoload_psr4.php
@@ -9,8 +9,6 @@ return array(
     'think\\app\\' => array($vendorDir . '/topthink/think-multi-app/src'),
     'think\\' => array($vendorDir . '/topthink/framework/src/think', $vendorDir . '/topthink/think-validate/src', $vendorDir . '/topthink/think-orm/src', $vendorDir . '/topthink/think-container/src', $vendorDir . '/topthink/think-helper/src'),
     'app\\' => array($baseDir . '/app'),
-    'Symfony\\Polyfill\\Mbstring\\' => array($vendorDir . '/symfony/polyfill-mbstring'),
-    'Symfony\\Component\\VarDumper\\' => array($vendorDir . '/symfony/var-dumper'),
     'Psr\\SimpleCache\\' => array($vendorDir . '/psr/simple-cache/src'),
     'Psr\\Log\\' => array($vendorDir . '/psr/log/src'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
